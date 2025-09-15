556c802dcaf72a3642edbef27455fe697d450fa8
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-09-15 13:08:20 +0100

    Address 'Line too long' Problem.

--- better_ships_that_battle_better/better_ships_that_battle_better/wsgi.py
+++ better_ships_that_battle_better/better_ships_that_battle_better/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'better_ships_that_battle_better.better_ships_that_battle_better.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
+'better_ships_that_battle_better.better_ships_that_battle_better.settings')
 
 application = get_wsgi_application()
