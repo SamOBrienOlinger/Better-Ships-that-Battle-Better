44b1985ccd064eafe214b584a7ce95363af21ae2
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-09-16 17:05:54 +0100

    git commit -m "Fix pirates app path in INSTALLED_APPS"

--- better_ships_that_battle_better/better_ships_that_battle_better/settings.py
+++ better_ships_that_battle_better/better_ships_that_battle_better/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pirates',
+    'better_ships_that_battle_better.pirates',
 ]
 
 MIDDLEWARE = [
