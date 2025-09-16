530ef7069532a10b9927aebf4368c1f45da2bb54
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-09-16 16:56:28 +0100

    Fix apps.py file

--- better_ships_that_battle_better/pirates/apps.py
+++ better_ships_that_battle_better/pirates/apps.py
@@ -1,6 +1,13 @@
+"""
+Django app configuration for the pirates application.
+
+This module contains the AppConfig class for the pirates app,
+which is part of the Better Ships that Battle Better game.
+"""
 from django.apps import AppConfig
 
 
 class PiratesConfig(AppConfig):
+    """Django app configuration for the pirates application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pirates'
